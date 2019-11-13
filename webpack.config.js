a59f5b11c67bf4a72d65a47cea8188809f1afd93
wendivue <vovas2345@gmail.com>
2019-11-13 21:12:49 +1000

    project setup

---
+++ webpack.config.js
@@ -0,0 +1,101 @@
+const path = require("path");
+const fs = require("fs");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+const config = {
+  entry: ["./src/js/index.js", "./src/scss/style.scss"],
+  output: {
+    filename: "./js/bundle.js"
+  },
+  devtool: "source-map",
+  mode: "production",
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        sourceMap: true,
+        extractComments: true
+      })
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sass|scss)$/,
+        include: path.resolve(__dirname, "src/scss"),
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {}
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+              url: false
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              sourceMap: true,
+              plugins: () => [
+                require("cssnano")({
+                  preset: [
+                    "default",
+                    {
+                      discardComments: {
+                        removeAll: true
+                      }
+                    }
+                  ]
+                })
+              ]
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.pug$/,
+        use: ["pug-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./css/style.bundle.css"
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: "./src/fonts",
+        to: "./fonts"
+      },
+      {
+        from: "./src/favicon",
+        to: "./favicon"
+      }
+    ]),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/pug/index.pug"
+    }),
+    
+  ]
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === "production") {
+    config.plugins.push(new CleanWebpackPlugin());
+  }
+  return config;
+};
