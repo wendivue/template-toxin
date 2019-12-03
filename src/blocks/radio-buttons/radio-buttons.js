e4c5be34739b1beb4f89b3244e0ed73e9189f6ff
wendivue <vovas2345@gmail.com>
2019-12-03 17:22:02 +1000

    add radio-buttons

---
+++ src/blocks/radio-buttons/radio-buttons.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $( function() {
+
+$(".radio-buttons__radio").on('click', function() {
+    $('.radio-buttons__label').toggleClass("radio-buttons__label--active")
+  });
+  
+    });
+});
--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,4 @@ import "air-datepicker/dist/js/datepicker";
 import "../blocks/dropdown/dropdown";
 import "../blocks/date-picker/date-picker";
 import "../blocks/checkbox-list/checkbox-list";
+import "../blocks/radio-buttons/radio-buttons";
