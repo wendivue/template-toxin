e0dd7cebf565f23e3fd4962ac7a4fc130f36ad0b
wendivue <vovas2345@gmail.com>
2019-12-12 19:20:52 +1000

    add range-slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import "air-datepicker/dist/js/datepicker";
+import "jquery-ui/ui/widgets/slider";
 
 import "../blocks/dropdown/dropdown";
 import "../blocks/date-picker/date-picker";
 import "../blocks/checkbox-list/checkbox-list";
 import "../blocks/radio-buttons/radio-buttons";
+import "../blocks/range-slider/range-slider";
