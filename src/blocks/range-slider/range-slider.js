6422098ced94c9e6a7a505b5cdcbcaa8d231027a
wendivue <vovas2345@gmail.com>
2020-12-04 21:41:37 +1000

    refactor(range-slider): change jquery plugin

--- src/blocks/range-slider/range-slider.js
+++ src/blocks/range-slider/range-slider.js
@@ -1,17 +1,6 @@
-$(document).ready(function() {
-
-    $( function() {
-        $( "#slider-range" ).slider({
-            range: true,
-            min: 0,
-            max: 15700,
-            values: [ "5000", "10000" ],
-            slide: function( event, ui ) {
-              $( "#amount" ).val(ui.values[0].toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ') + '\u{20BD}' + " - "+
-               ui.values[1].toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ') + '\u{20BD}');
-            }
-          });
-          $( "#amount" ).val($( "#slider-range" ).slider( "values", 0 ).toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ') +
-          '\u{20BD}' + " - " + $( "#slider-range" ).slider( "values", 1 ).toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ') + '\u{20BD}');
-    });
+$('.range-slider__input').ionRangeSlider({
+  type: 'double',
+  postfix: '₽',
+  step: 100,
+  values_separator: ' - ',
 });
