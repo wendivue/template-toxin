ecd83b215ec0f5904b4db63254532cf571054238
wendivue <vovas2345@gmail.com>
2020-12-04 18:30:56 +1000

    stylo(hotel-card): indentation and single quotes

--- src/blocks/hotel-card/hotel-card.js
+++ src/blocks/hotel-card/hotel-card.js
@@ -1,11 +1,9 @@
-$(document).ready(function() {
-    $('.owl-carousel').owlCarousel({
-        nav:true,
-        items: 1,
-        dots: true,
-        navText: [
-            '<i class="material-icons">expand_more</i>',
-            '<i class="material-icons">expand_more</i>'
-        ]
-      })
+$('.owl-carousel').owlCarousel({
+  nav: true,
+  items: 1,
+  dots: true,
+  navText: [
+    '<i class="material-icons">expand_more</i>',
+    '<i class="material-icons">expand_more</i>',
+  ],
 });
